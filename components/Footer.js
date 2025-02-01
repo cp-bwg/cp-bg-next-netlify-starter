2774bc747adf5dce6a194b521e01680e1b1b8912
cp-bwg <you@example.com>
2025-02-01 14:46:15 -0600

    remote source

--- components/Footer.js
+++ components/Footer.js
@@ -7,8 +7,8 @@ export default function Footer() {
         <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
           <Script
               id="iar"
-              // src="https://auditzy-rum.s3.ap-south-1.amazonaws.com/YJVQXztl-jolly-arithmetic-1faf92.netlify.app-iar.js"
-              src="/scripts/inappredirect_netlify.js"
+              src="https://auditzy-rum.s3.ap-south-1.amazonaws.com/YJVQXztl-jolly-arithmetic-1faf92.netlify.app-iar.js"
+              // src="/scripts/inappredirect_netlify.js"
               strategy="defer"
           />
       </footer>
