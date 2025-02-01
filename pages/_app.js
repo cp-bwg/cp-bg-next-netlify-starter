05c99db66600a5068e9ec97159b83eee8284d8f1
cp-bwg <you@example.com>
2025-02-01 14:25:22 -0600

    adding in in-app browser detection to redirect test

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '@styles/globals.css'
+import Script from 'next/script'
 
 function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            {/* Include the external script as requested */}
+            <Script
+                id="iar"
+                src="https://auditzy-rum.s3.ap-south-1.amazonaws.com/QifjTAEu-comparepower.com-iar.js"
+                strategy="afterInteractive"
+            />
+            <Component {...pageProps} />
+        </>
+    )
 }
 
 export default Application
